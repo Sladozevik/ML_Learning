432abf024eb71e7c54e1bfdb1500543c3bbef30c
Ante Sladojevic <asladojevic@outlook.com>
2016-11-11 13:43:59 +0100

    added Machine Learning file where i trained with 3 ML algorithms: 1. KNeighborsClassifier ( k-nearest neighbors ) 2. AdaBoostClassifier 3. RandomForestClassifier

--- n_NN.py
+++ n_NN.py
@@ -16,9 +16,14 @@ def n_nn(X_train, y_train, X_test, y_test):
     neigh = KNeighborsClassifier(n_neighbors=3)
     neigh.fit(X_train, y_train) 
     pred_nnn = neigh.predict(X_test)
-#    print pred_nnn
     print('n nn Accuracy is: ', accuracy_score(y_test, pred_nnn))
 
+def n_nn_tun(X_train, y_train, X_test, y_test):
+    neight_tun = KNeighborsClassifier(n_neighbors = 1)
+    neight_tun.fit(X_train, y_train) 
+    pred_nnn_tun = neight_tun.predict(X_test)
+    print('n nn Accuracy Tuned is: ', accuracy_score(y_test, pred_nnn_tun))
+    
 def adaboost(X_train, y_train, X_test, y_test):
     ad_boo = AdaBoostClassifier()
     ad_boo.fit(X_train, y_train)
@@ -31,6 +36,14 @@ def random_forest(X_train, y_train, X_test, y_test):
     pred_ran_for = ran_for.predict(X_test)
     print('Random Fores Accuracy is: ', accuracy_score(y_test, pred_ran_for))
     
+def random_forest_accuracy(X_train, y_train, X_test, y_test):
+    ran_for_tun = RandomForestClassifier(n_estimators=100, criterion='gini')
+    ran_for_tun.fit(X_train, y_train)
+    pred_ran_for_tun = ran_for_tun.predict(X_test)
+    print('Random Fores Tuned Accuracy is: ', accuracy_score(y_test, pred_ran_for_tun))
+
 n_nn(X_train, y_train, X_test, y_test)
+n_nn_tun(X_train, y_train, X_test, y_test)
 adaboost(X_train, y_train, X_test, y_test)
 random_forest(X_train, y_train, X_test, y_test)
+random_forest_accuracy(X_train, y_train, X_test, y_test)
