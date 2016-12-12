be129467307667982e1f29838eec44654dd4da01
Ante Sladojevic <asladojevic@outlook.com>
2016-12-12 15:37:22 +0100

    adding new files

--- n_NN.py
+++ n_NN.py
@@ -16,31 +16,31 @@ def n_nn(X_train, y_train, X_test, y_test):
     neigh = KNeighborsClassifier(n_neighbors=3)
     neigh.fit(X_train, y_train) 
     pred_nnn = neigh.predict(X_test)
-    print('n nn Accuracy is: ', accuracy_score(y_test, pred_nnn))
+    print('n nn Accuracy is: ', round(accuracy_score(y_test, pred_nnn),2))
 
 def n_nn_tun(X_train, y_train, X_test, y_test):
     neight_tun = KNeighborsClassifier(n_neighbors = 1)
     neight_tun.fit(X_train, y_train) 
     pred_nnn_tun = neight_tun.predict(X_test)
-    print('n nn Accuracy Tuned is: ', accuracy_score(y_test, pred_nnn_tun))
+    print('n nn Accuracy Tuned is: ', round(accuracy_score(y_test, pred_nnn_tun),2))
     
 def adaboost(X_train, y_train, X_test, y_test):
     ad_boo = AdaBoostClassifier()
     ad_boo.fit(X_train, y_train)
     pred_ad_boo = ad_boo.predict(X_test)
-    print('AdaBoost Accurcy is: ', accuracy_score(y_test, pred_ad_boo))
+    print('AdaBoost Accurcy is: ', round(accuracy_score(y_test, pred_ad_boo),2))
 
 def random_forest(X_train, y_train, X_test, y_test):
     ran_for = RandomForestClassifier()
     ran_for.fit(X_train, y_train)
     pred_ran_for = ran_for.predict(X_test)
-    print('Random Fores Accuracy is: ', accuracy_score(y_test, pred_ran_for))
+    print('Random Fores Accuracy is: ', round(accuracy_score(y_test, pred_ran_for),2))
     
 def random_forest_accuracy(X_train, y_train, X_test, y_test):
     ran_for_tun = RandomForestClassifier(n_estimators=100, criterion='gini')
     ran_for_tun.fit(X_train, y_train)
     pred_ran_for_tun = ran_for_tun.predict(X_test)
-    print('Random Fores Tuned Accuracy is: ', accuracy_score(y_test, pred_ran_for_tun))
+    print('Random Fores Tuned Accuracy is: ', round(accuracy_score(y_test, pred_ran_for_tun),2))
 
 n_nn(X_train, y_train, X_test, y_test)
 n_nn_tun(X_train, y_train, X_test, y_test)
