16383c49160d633524eb764d2557fff833537775
Ante Sladojevic <asladojevic@outlook.com>
2017-01-14 23:13:05 +0100

    added check tensorflow file

--- Check TensorFlow.py
+++ Check TensorFlow.py
@@ -25,3 +25,19 @@ print('pandas: {}'.format(pandas.__version__))
 # scikit-learn
 import sklearn
 print('sklearn: {}'.format(sklearn.__version__))
+
+import numpy as np
+import tensorflow as tf
+import math
+
+def add_one():
+    with tf.Session() as session:
+        # (1)
+        x = tf.placeholder(tf.float32, [1], name='x') # fed as input below
+        y = tf.placeholder(tf.float32, [1], name='y') # fetched as output below
+        b = tf.constant(1.0)
+        y = x + b # here is our ‘model’: add one to the input.
+        x_in = [2] # (2)
+        y_final = session.run([y], {x: x_in}) # (3)
+    print(y_final) # (4)
+add_one()
